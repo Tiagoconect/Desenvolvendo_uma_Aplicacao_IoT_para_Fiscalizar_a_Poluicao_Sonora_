c6bc769a9228cdd1ac32686b07e65e89d57c206b
Tiago Nascimento Ferreira <tiagodois.0@gmail.com>
2023-11-06 16:43:48 -0300

    dando alguns retorques

--- media_movel.py
+++ media_movel.py
@@ -6,21 +6,42 @@ import pandas as pd  # pandas para usar o formato de data/hora
 
 # Conectando ao MongoDB
 client = MongoClient("mongodb+srv://tiagodois0:80432162@cluster0.zxazakw.mongodb.net/test")
-db = client.cluster0  # Replace with the correct database name
+db = client.cluster0  # Substitua pelo nome correto do banco de dados
 
-# solicitando dados da coleção do banco de dados que estão os valores da enviados pela esp
-collection = db["valores_enviados_pela_esp"]  
+# Solicitando dados da coleção do banco de dados onde os valores enviados pela ESP estão armazenados
+collection = db["valores_enviados_pela_esp"]
 
-# Variáveis locais para receber valores do sensor e tempo no MongoDB
+# Variáveis locais para receber valores do sensor, tempo e ângulo do servo no MongoDB
 sensor_data = []
 timestamps = []
+servo_angles = []
+rolling_window = 10  # Janela para a média móvel
 
 for doc in collection.find().sort("tempo", 1):
-    sensor_data.append(float(doc["valor_sensor"]))
-    timestamps.append(doc["tempo"])  
+    sensor_value = float(doc["valor_sensor"])
+    timestamp = pd.to_datetime(doc["tempo"])
+    servo_angle = float(doc["valor_servo"])
 
-# Convertendo o formato de dados com pandas 
-timestamps = [pd.to_datetime(ts) for ts in timestamps]
+    sensor_data.append(sensor_value)
+    timestamps.append(timestamp)
+    servo_angles.append(servo_angle)
+
+    # Calculando a média móvel com base no novo valor do sensor
+    if len(sensor_data) > rolling_window:
+        sensor_data_avg = np.mean(sensor_data[-rolling_window:])
+    else:
+        sensor_data_avg = sensor_value
+
+    # Criando um documento com os valores para inserir na coleção
+    document = {
+        "tempo": timestamp,
+        "valor_sensor": sensor_value,
+        "media_movel": sensor_data_avg,
+        "angulo_servo": servo_angle
+    }
+
+    # Inserindo o documento na coleção
+    db["valores_media_movel"].insert_one(document)
 
 # Usando um estilo de gráfico de fundo escuro
 plt.style.use('dark_background')
@@ -34,8 +55,10 @@ plt.title('Valores do Sensor ao Longo do Tempo')
 plt.gca().xaxis.set_major_formatter(DateFormatter("%Y-%m-%d %H:%M:%S"))
 plt.xticks(rotation=45)
 
+# Continuar com o restante do código para criar o gráfico de média móvel e salvar/exibir o gráfico
+# ...
+
 # Calculando a média móvel em função do tempo e plotando junto com o valor do sensor em função do tempo
-rolling_window = 10 
 sensor_data_avg = np.convolve(sensor_data, np.ones(rolling_window) / rolling_window, mode='same')
 plt.plot(timestamps, sensor_data_avg, linestyle='-', label=f'Média (intervalo_amostral={rolling_window})', color='red')
 
--- tratando_testeH.py
+++ tratando_testeH.py
@@ -1,75 +1,66 @@
-# Importando bibliotecas necessarias
 from pymongo import MongoClient
-import numpy as np
-from scipy import stats
-import matplotlib.pyplot as plt
+import pandas as pd
 
 # Conectando ao MongoDB
 client = MongoClient("mongodb+srv://tiagodois0:80432162@cluster0.zxazakw.mongodb.net/test")
-db = client.cluster0  # escolha do banco de dados de acesso do projeto
+db = client.cluster0  # Escolha o banco de dados do projeto
 
-# iniciando a função para analizar as anormalia o intervalo amostral, percental e a posição levando en conta o zcalq
-def calcular_percentagem_anomalias(dados_sensor, z_critico):
-    angulos_servo = set(dado["valor_servo"] for dado in dados_sensor)
-    contagens_anomalias = {angulo: [] for angulo in angulos_servo}
+# Coleção com os valores da média móvel
+colecao_media_movel = db["valores_media_movel"]
+
+# Coleção com os resultados do teste H (tratando_testeH)
+colecao_tratando_testeH = db["tratando_testeH"]
+
+# Função para verificar se um valor está dentro dos limites de ruído permitidos
+def verificar_limite_ruido(valor, periodo):
+    limite_diurno = 55  # Limite diurno em decibéis
+    limite_noturno = 50  # Limite noturno em decibéis
+
+    if periodo == "diurno":
+        return valor <= limite_diurno
+    elif periodo == "noturno":
+        return valor <= limite_noturno
+
+# Função para verificar se um valor anormal viola a legislação de ruído
+def verificar_violacao_legislacao(angulo_servo, media_movel, tempo):
+    if 7 <= tempo.hour < 20:
+        periodo = "diurno"
+    else:
+        # Verifique se o próximo dia é domingo ou feriado
+        proximo_dia = tempo + pd.DateOffset(days=1)
+        if proximo_dia.weekday() == 6 or proximo_dia in feriados:
+            if 20 <= tempo.hour < 9:
+                periodo = "noturno"
+        else:
+            if 20 <= tempo.hour < 7:
+                periodo = "noturno"
+
+    if verificar_limite_ruido(media_movel, periodo):
+        return f"Ângulo {angulo_servo} às {tempo}: {media_movel} dB no período {periodo} viola a legislação"
+    else:
+        return None
+
+# Iterar pela coleção de valores da média móvel
+for doc in colecao_media_movel.find():
+    angulo_servo = doc["valor_servo"]
+    media_movel = doc["media_movel"]
+    tempo = doc["tempo"]
     
-    for dado in dados_sensor:
-        escore_z = abs((dado["valor_sensor"] - media) / desvio_padrao)
-        if escore_z > z_critico:
-            contagens_anomalias[dado["valor_servo"]].append(dado)
+    # Verificar se há algum valor anormal na coleção tratando_testeH no mesmo horário
+    query = {
+        "servo_angle": angulo_servo,
+        "anomalous_values.tempo": tempo
+    }
+    resultado = colecao_tratando_testeH.find_one(query)
     
-    return contagens_anomalias
-
-# selecionando a coleção que tem os valores do sensor, tempo e angulo do servo
-colecao = db["valores_enviados_pela_esp"] 
-
-# Restrigindo o intervalo de coleta das ultimas 70 amsostras do banco de dados
-dados_sensor = list(colecao.find().sort([("tempo", -1)]).limit(70))
-
-# convertendo os valores do sensor de string para float
-for dado in dados_sensor:
-    dado["valor_sensor"] = float(dado["valor_sensor"])
-
-# Calculando media e descio padrão
-media = np.mean([dado["valor_sensor"] for dado in dados_sensor])  # media
-desvio_padrao = np.std([dado["valor_sensor"] for dado in dados_sensor])  # desvio pardao
-
-#definindo o valor de alfa
-alfa = 0.1
-
-# Calcula o z critico
-z_critico = stats.norm.ppf(1 - alfa / 2)
-
-# Calcula as anomalias nas últimas 70 amostras
-anomalous_values_by_angle = calcular_percentagem_anomalias(dados_sensor, z_critico)
-
-# Crie uma nova coleção chamada "tratando_testeH"
-tratando_testeH = db["tratando_testeH"]
-
-#Armazena os resultados na nova coleção
-for angulo, anomalous_values in anomalous_values_by_angle.items():
-    percentagem = (len(anomalous_values) / 10) * 100
-    print(f"Servo Angle {angulo}: {percentagem:.2f}% anomalies")
-    print("Anomalous Values:")
-    for anomalous_value in anomalous_values:
-        print(anomalous_value)
-    
-    # criando uma nova coleção para inserir os valores significativos da analise do testeH em função da posição do servo
-    tratando_testeH.insert_one({
-        "servo_angle": angulo,
-        "percentage_anomalies": percentagem,
-        "anomalous_values": anomalous_values
-    })
-
-# criando grafico para ver a taxa de anarmalidade em um intervalo amoistral de 70 amostras e um função da podição do servo
-angulos = list(anomalous_values_by_angle.keys())
-percentagens = [(len(values) / 10) * 100 for values in anomalous_values_by_angle.values()]
-
-plt.bar(angulos, percentagens)
-plt.xlabel("Angulo do Servo")
-plt.ylabel("Porcentagem ode valores anormais (%)")
-plt.title("Porcentagem de valores anormais por angulo do servo")
-
-plt.show()
-
-
+    if resultado:
+        anomalous_values = resultado["anomalous_values"]
+        
+        for anomalous_value in anomalous_values:
+            tempo_anomalia = anomalous_value["tempo"]
+            
+            # Verificar se o valor anormal também viola a legislação
+            violacao = verificar_violacao_legislacao(angulo_servo, media_movel, tempo_anomalia)
+            
+            if violacao:
+                print(violacao)
